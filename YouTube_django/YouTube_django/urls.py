8abd7344a3ef1de089bdfc1cb6d49fb4e4c0f858
Sona Gevorgyan <gevorgyans325@gmail.com>
2021-02-21 22:41:57 +0400

    YouTube

--- YouTube_django/YouTube_django/urls.py
+++ YouTube_django/YouTube_django/urls.py
@@ -22,6 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('download_app.urls')),
     path('users/', include('users.urls')),
-
 ]
+
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
